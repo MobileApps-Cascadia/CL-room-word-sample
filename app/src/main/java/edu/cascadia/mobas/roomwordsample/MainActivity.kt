545f58639e4bf93c9e39d646416653173546cbc8
Brian Bansenauer <brianb@cascadia.edu>
2022-12-28 22:10:25 -0800

    updates view binding

--- app/src/main/java/edu/cascadia/mobas/roomwordsample/MainActivity.kt
+++ app/src/main/java/edu/cascadia/mobas/roomwordsample/MainActivity.kt
@@ -2,10 +2,16 @@ package edu.cascadia.mobas.roomwordsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.cascadia.mobas.roomwordsample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
