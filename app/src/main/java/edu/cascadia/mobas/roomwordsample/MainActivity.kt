affecc8e0643cc5158685fdcf235e3c389ffd6f9
Tori Lew <torirlew@gmail.com>
2023-01-23 18:21:32 -0800

    Android Room with a View # 13-15. I need to work on committing more often and per the Canvas instructions

--- app/src/main/java/edu/cascadia/mobas/roomwordsample/MainActivity.kt
+++ app/src/main/java/edu/cascadia/mobas/roomwordsample/MainActivity.kt
@@ -2,11 +2,15 @@ package edu.cascadia.mobas.roomwordsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import edu.cascadia.mobas.roomwordsample.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
