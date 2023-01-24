967edde54ae55299c974b03838ea4e7d0c783d03
Tori Lew <torirlew@gmail.com>
2023-01-23 21:25:57 -0800

    View binding is now working correctly

--- app/src/main/java/edu/cascadia/mobas/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/edu/cascadia/mobas/roomwordsample/NewWordActivity.kt
@@ -5,26 +5,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.Button
-import android.widget.EditText
-import edu.cascadia.mobas.roomwordsample.databinding.ActivityMainBinding
+import edu.cascadia.mobas.roomwordsample.databinding.ActivityNewWordBinding
 
-private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityNewWordBinding
 
 class NewWordActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityNewWordBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val editWordView = findViewById<EditText>(R.id.edit_word)
-        binding.fab.setOnClickListener {
+        binding.buttonSave.setOnClickListener {
             val replyIntent = Intent()
-            if (TextUtils.isEmpty(editWordView.text)) {
+            if (TextUtils.isEmpty(binding.editWord.editableText)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
             } else {
-                val word = editWordView.text.toString()
+                val word = binding.editWord.editableText.toString()
                 replyIntent.putExtra(EXTRA_REPLY, word)
                 setResult(Activity.RESULT_OK, replyIntent)
             }
