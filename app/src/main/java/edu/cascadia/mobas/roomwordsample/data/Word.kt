d33aca7353996e5bee87c4ef048baae6e0de9ac7
Kenny <kennypluchau@gmail.com>
2023-01-23 22:12:22 -0800

    Implimented Room database

--- app/src/main/java/edu/cascadia/mobas/roomwordsample/data/Word.kt
+++ app/src/main/java/edu/cascadia/mobas/roomwordsample/data/Word.kt
@@ -1,4 +1,11 @@
 package edu.cascadia.mobas.roomwordsample.data
 
-class Word {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "word") val word: String
+)
