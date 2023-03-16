65c0195e7842790201c974b4adfcd15aa9c59c1e
asb43 <pokemonfanboy600@gmail.com>
2023-03-15 17:22:22 -0700

    completed the assignment

---
+++ app/src/main/java/edu/cascadia/mobas/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,35 @@
+package edu.cascadia.mobas.roomwordsample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import edu.cascadia.mobas.roomwordsample.databinding.ActivityNewWordBinding
+
+private lateinit var binding: ActivityNewWordBinding
+
+class NewWordActivity : AppCompatActivity() {
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewWordBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.buttonSave.setOnClickListener{
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(binding.editWord.editableText)) {
+                setResult(RESULT_CANCELED, replyIntent)
+            } else {
+                val word = binding.editWord.editableText.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
--- app/src/main/java/edu/cascadia/mobas/roomwordsample/WordRepository.kt
+++ app/src/main/java/edu/cascadia/mobas/roomwordsample/WordRepository.kt
@@ -1,4 +1,17 @@
 package edu.cascadia.mobas.roomwordsample
 
-class WordRepository {
+import androidx.annotation.WorkerThread
+import edu.cascadia.mobas.roomwordsample.data.Word
+import edu.cascadia.mobas.roomwordsample.data.WordDao
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
 }
